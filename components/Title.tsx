2a9fd7117df420d2c94530e35670b28e26991d57
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-09-30 09:51:17 +0300

    Improve logo

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,29 +1,39 @@
+import { Center } from '@mantine/core';
 import { YEAR } from '~/lib/config';
 import classes from './Title.module.css';
 
 export function Title() {
   return (
-    <h1 className={classes.root}>
-      <svg className={classes.logo} width="24" height="24" viewBox="0 0 24 24" fill="none">
-        <path fill="#CE1126" d="M17.5 10H14v9h5l-1.5-9Z" />
-        <path fill="#FCD116" d="M10 10h4v9h-4z" />
-        <path fill="#002B7F" d="M6.5 10H10v9H5l1.5-9Z" />
+    <Center component="h1" className={classes.root}>
+      <svg
+        className={classes.logo}
+        width="32"
+        height="32"
+        viewBox="0 0 32 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M23.6 13H18V26H26L23.6 13Z" fill="#e73145" />
+        <rect x="14" y="13" width="4" height="13" fill="#fcd116" />
+        <path d="M8.4 13H14V26H6L8.4 13Z" fill="#2659bc" />
         <path
-          fill="currentColor"
-          fillRule="evenodd"
-          d="M9.172 3.172a4 4 0 1 1 5.656 5.656 4 4 0 0 1-5.656-5.656ZM12 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z"
-          clipRule="evenodd"
+          d="M12 8C12 9.06087 12.4214 10.0783 13.1716 10.8284C13.9217 11.5786 14.9391 12 16 12C17.0609 12 18.0783 11.5786 18.8284 10.8284C19.5786 10.0783 20 9.06087 20 8C20 6.93913 19.5786 5.92172 18.8284 5.17157C18.0783 4.42143 17.0609 4 16 4C14.9391 4 13.9217 4.42143 13.1716 5.17157C12.4214 5.92172 12 6.93913 12 8Z"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
-          fill="currentColor"
-          fillRule="evenodd"
-          d="M6.835 8a2 2 0 0 0-1.968 1.642l-1.637 9A2 2 0 0 0 5.198 21h13.604M6.835 8h10.33a2 2 0 0 1 1.968 1.642l1.637 9A1.999 1.999 0 0 1 18.802 21M6.835 10l-1.637 9h13.604l-1.637-9H6.835Z"
-          clipRule="evenodd"
+          d="M9.11333 12H22.8867C23.1989 12 23.5013 12.1095 23.7411 12.3095C23.9808 12.5096 24.1428 12.7875 24.1987 13.0947L26.3813 25.0947C26.4163 25.2868 26.4086 25.4843 26.3588 25.6732C26.309 25.8621 26.2183 26.0377 26.0932 26.1877C25.968 26.3376 25.8115 26.4583 25.6345 26.541C25.4576 26.6238 25.2647 26.6667 25.0693 26.6667H6.93067C6.73534 26.6667 6.54239 26.6238 6.36547 26.541C6.18855 26.4583 6.03197 26.3376 5.90681 26.1877C5.78165 26.0377 5.69097 25.8621 5.64118 25.6732C5.59139 25.4843 5.5837 25.2868 5.61867 25.0947L7.80133 13.0947C7.85723 12.7875 8.01917 12.5096 8.25894 12.3095C8.4987 12.1095 8.80107 12 9.11333 12Z"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
       <span className={classes.text}>
-        <span className={classes.dynamicText}>Taxe </span>PFA<span className={classes.dynamicText}> în</span> {YEAR}
+        <span className={classes.dynamicText}>Taxe </span>PFA în {YEAR}
       </span>
-    </h1>
+    </Center>
   );
 }
