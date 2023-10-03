721c777a43f95e2c28274eb30126bd266776f918
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-10-03 17:32:33 +0300

    Add more currencies

--- lib/config.ts
+++ lib/config.ts
@@ -8,7 +8,7 @@ export const PENSION_PERCENTAGE = 0.25;
 export const HEALTH_PERCENTAGE = 0.1;
 export const INCOME_TAX_PERCENTAGE = 0.1;
 export const BASE_CURRENCY = 'RON';
-export const CURRENCIES = [BASE_CURRENCY, 'EUR', 'USD', 'GBP', 'CHF'];
+export const CURRENCIES = [BASE_CURRENCY, 'EUR', 'USD', 'GBP', 'CHF', 'CAD', 'AUD'];
 export const VAT_THRESHOLD = 300_000;
 export const EXCHANGE_RATES_RELOAD_INTERVAL = 3_600_000;
 
