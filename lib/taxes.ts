bc3c3f3f9c242cda176f6b650b3a0a0af6379868
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-10-04 13:11:50 +0300

    Add comments to taxes calculation

--- lib/taxes.ts
+++ lib/taxes.ts
@@ -30,6 +30,7 @@ export function calculateTaxes({
   )
     return;
 
+  // subtract vacation time and adjust the income accordingly
   if (incomeInterval === 'hourly') income *= workingHoursPerWeek * (WEEKS_PER_YEAR - vacationWeeksPerYear);
   else if (incomeInterval === 'daily') income *= workingDaysPerWeek * (WEEKS_PER_YEAR - vacationWeeksPerYear);
   else if (incomeInterval === 'monthly') income *= 12 - vacationWeeksPerYear / WEEKS_PER_MONTH;
@@ -37,31 +38,41 @@ export function calculateTaxes({
   // people could accidentally input too many vacation weeks, so we need to make sure the income is not negative
   income = Math.max(income, 0);
 
+  // convert income to base currency if needed
   if (incomeCurrency !== BASE_CURRENCY) income *= exchangeRates![incomeCurrency];
 
+  // save the gross income to return it later
   const grossIncome = income;
 
   if (deductibleExpensesInterval === 'monthly') deductibleExpenses *= 12;
+
+  // convert deductible expenses to base currency if needed
   if (deductibleExpensesCurrency !== BASE_CURRENCY) deductibleExpenses *= exchangeRates![deductibleExpensesCurrency];
 
+  // subtract deductible expenses from the income
   income -= deductibleExpenses;
 
+  // calculate the pension tax amount and percentage of the gross income
   let pensionTaxAmount = 0;
   if (income >= minimumWage * 24) pensionTaxAmount = minimumWage * 24 * PENSION_PERCENTAGE;
   else if (income >= minimumWage * 12) pensionTaxAmount = minimumWage * 12 * PENSION_PERCENTAGE;
   const pensionTaxPercentage = grossIncome === 0 ? 0 : (pensionTaxAmount / grossIncome) * 100;
 
+  // calculate the health tax amount and percentage of the gross income
   let healthTaxAmount = 0;
   if (income >= minimumWage * 60) healthTaxAmount = minimumWage * 60 * HEALTH_PERCENTAGE;
   else if (income >= minimumWage * 6) healthTaxAmount = income * HEALTH_PERCENTAGE;
   else healthTaxAmount = minimumWage * 6 * HEALTH_PERCENTAGE;
   const healthTaxPercentage = (healthTaxAmount / grossIncome) * 100;
 
+  // calculate the taxable income and make sure it's not negative
   const taxableIncome = Math.max(income - pensionTaxAmount - healthTaxAmount - deductibleExpenses, 0);
 
+  // calculate the income tax amount and percentage of the gross income
   const incomeTaxAmount = taxableIncome * INCOME_TAX_PERCENTAGE;
   const incomeTaxPercentage = grossIncome === 0 ? 0 : (incomeTaxAmount / grossIncome) * 100;
 
+  // calculate the total tax amount and percentage of the gross income
   const totalTaxAmount = pensionTaxAmount + healthTaxAmount + incomeTaxAmount;
   const totalTaxPercentage = (totalTaxAmount / grossIncome) * 100;
 
